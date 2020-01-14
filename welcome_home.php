a1e5cb2872e7e9dea7c8235554b9b53d39794b49
Pankhuri1999 <petal1999@gmail.com>
2020-01-15 00:51:42 +0530

    added welcome_home.php

---
+++ welcome_home.php
@@ -0,0 +1,227 @@
+
+<!DOCTYPE HTML>
+<HEAD>
+<TITLE>home_page</TITLE>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</HEAD>
+<BODY background="pro.jpg">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+
+ {font-family: Arial, Helvetica, sans-serif;
+}
+
+.navbar {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.navbar a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+.navbar a:hover ,{background-color:#A93226;
+}
+
+.accordion {
+  background-color: #eee;
+  color: #444;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+
+}
+
+.active, .accordion:hover {
+  background-color: #ccc;
+}
+
+.panel {
+  padding: 0 18px;
+  display: none;
+  background-color: white;
+  overflow: hidden;
+}
+ {
+  box-sizing: border-box;
+}
+
+video {
+  width: 50%;
+  height: auto;
+}
+hr {
+  display: block;
+  margin-top: 0.5em;
+  margin-bottom: 0.5em;
+  margin-left: auto;
+  margin-right: auto;
+  border-style: inset;
+  border-width: 1px;
+}
+
+.fa {
+  padding: 20px;
+  font-size: 30px;
+  width: 50px;
+  text-align: center;
+  text-decoration: none;
+}
+
+/* Add a hover effect if you want */
+.fa:hover {
+  opacity: 0.7;
+}
+
+/* Set a specific color for each brand */
+
+/* Facebook */
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+
+/* Twitter */
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+
+.fa-linkedin {
+  background: #0077B5;
+  color: white;
+}
+</style>
+</head>
+<body>
+
+<?php
+ // welcome home page , about us _ONGC
+
+ ?>
+
+
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+</script>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<br>
+
+<br>
+
+ <div class="navbar">
+	<a href="article.php">Articles</a>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!-- Add font awesome icons -->
+
+
+</div>
+
+
+<hr>
+<div align="center">
+<h1 = font size:100><b><u>
+ARTICLE SHARING</b></u></h1>
+</div>
+<pre>
+  <div class="container" align="center"><pre><h3><font color="#000000">
+
+    Start writing your articles :)
+
+    <video width="10" height="10" controls>
+<source src="https://www.youtube.com/watch?v=t9ixkSlQlss.mp4" type="video/mp4"><sour src="https://www.youtube.com/watch?v=t9ixkSlQlss.ogg" type="video/ogg">
+</video>
+
+
+
+</pre>
+</fieldset>
+
+<br>
+<br>
+<pre>
+<body>
+<div style="float: left;">
+
+</video></div>
+
+
+<button class="accordion"><b><h3><font color="#A93226">Creativity and brain storming</font></b></h3></button>
+<div class="panel">
+Writing can also unlock some of the important thoughts you might otherwise dismiss or lose. Ordinarily, our minds are buzzing with random thoughts and ideas, but we tend to let them go easily because we view them as unimportant. When writing, you’re forced to evaluate your ideas with deliberate attentiveness, sorting through them in an attempt to find the perfect ones for your piece. This is also beneficial when you’re on the lookout for new topics and generating possible new angles for development; the creative process here can help you improve your creative processes in other areas of your profession, such as coming up with innovative solutions to pressing problems.</div>
+
+<button class="accordion"><b><h3><font color="#A93226">Better memory</font></b></h3></button>
+<div class="panel">
+There’s evidence to suggest that writing information down helps us retain it better in the future, which is especially important in the modern era. Because most facts and basic information is available to us after a quick Google search, our minds have adapted to become poorer at creating new memories; writing can help combat this. Whatever you write will have a higher chance of sticking in your head than anything you read or found on the Internet. Plus, writing is a mental exercise; just as your muscles need exercise to remain strong and durable, your mind needs exercise to remain in good shape too. Writing frequently and challenging yourself will help you store and recall more information from other areas of your life
+
+</div>
+<button class="accordion"><b><h3><font color="#A93226">More articulate converations</font></b></h3></button>
+<div class="panel">
+Writing more frequently will challenge you to develop and refine your vocabulary, as well as how you put your sentences together. You may have a fantastic idea in your head, but you can’t just give that idea to your readers; you need to find the perfect words to define and describe that idea, and use those to carry your message. In writing, this process takes time because you’re afforded the luxury of contemplation. When you first start, you may find it difficult to find the right words to describe your thoughts, but over time, you’ll become faster and more efficient at it. In line with this development, you’ll become a better conversationalist—you’ll be able to recall words faster, draw from a wider diversity of words, and put them together in ways that people understand more easily.
+</div>
+<button class="accordion"><b><h3><font color="#A93226">New knowledge</font></b></h3></button>
+<div class="panel">
+One of the most important and easiest benefits to acquire is the gathering of new information. Unless you’re already a certified expert in a given topic, you’ll need to do some in-depth research in order to write knowledgeably about it. When searching for new potential topics to write, you’ll become exposed to dozens, if not hundreds of new angles from publishers in your industry and your competitors, and if you pay attention to them, your perspective will expand. Beyond that, the more research you do, the more intelligent and better informed you’re going to be. The best content out there is material that’s been exhaustively researched, so make it a point to include facts and statistics you didn’t know before you started writing the article—it’s an easy way to keep yourself sharp
+</div>
+<button class="accordion"><b><h3><font color="#A93226">CATHARSIS</font></b></h3></button>
+<div class="panel">
+Expression in any form can be a powerful way to relieve stress and work through tough personal problems. Even if you’re writing about a topic related to your industry or your profession, you can choose topics that might be related to a current struggle. For example, if you’re a small business owner and you’re struggling with maintaining a positive cash flow, you could write an article to other entrepreneurs about how to keep your cash flow under control, or even how to remain calm in a crisis situation. Writing forces you to process your thoughts and feelings, which makes them easier to understand and live with. It’s a critical function of writing that shouldn’t be ignored
+</div>
+
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+</script>
+
+</pre>
+</body>
+</HTML>
+
+
